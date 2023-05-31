56c0370180c6748df6d22b757c0d912f462595e0
tj <thinupavani@gmail.com>
2023-05-31 11:54:14 +0530

    update

--- pages/api/speech_recognition.ts
+++ pages/api/speech_recognition.ts
@@ -9,7 +9,7 @@ console.log("chat id : ", chatId)
 console.log("apiType : ", apiType)
 
   try {
-    const response = await fetch('https://solutions.it-marketing.website/recording-start', {
+    const response = await fetch('https://solutions.it-marketing.website/test-audio', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
